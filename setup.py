d23b12a6dc65fb4d779da3a2b7f775c4a1df8ce6
Matteo Filipponi <mfilipponi@devicegateway.com>
2018-07-26 16:03:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='envutils',
+    version='1.0.0',
+    packages=['envutils'],
+    url='https://github.com/mfilippo/envutils',
+    license='MIT',
+    author='Matteo Filipponi',
+    author_email='matteofilipponi@hotmail.com',
+    description='A python library to read and parse environment variables.'
+)
