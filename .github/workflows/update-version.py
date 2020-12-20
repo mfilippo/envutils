8549fa60905f2724148b432e6940487434777580
Matteo Filipponi <mfilippo.dev@proton.me>
2020-12-20 17:54:23 +0100

    Strip v from version

--- .github/workflows/update-version.py
+++ .github/workflows/update-version.py
@@ -1,6 +1,8 @@
 import sys
 
 version = sys.argv[1]
+if version.lower().startswith("v"):
+    version = version[1:]
 version_filename = "envutils/version.py"
 with open(version_filename, "w") as f:
     f.write("__version__ = '{}'".format(version))
